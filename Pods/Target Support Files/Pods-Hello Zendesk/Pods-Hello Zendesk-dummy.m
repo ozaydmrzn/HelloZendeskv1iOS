0fb88bcfb77874c003d59426d24ba81e103d7998
Ozay Demirezen <demirezen@gmail.com>
2019-04-26 11:00:04 +0100

    Committing Pod Files

---
+++ Pods/Target Support Files/Pods-Hello Zendesk/Pods-Hello Zendesk-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Hello_Zendesk : NSObject
+@end
+@implementation PodsDummy_Pods_Hello_Zendesk
+@end
---
+++ Pods/Target Support Files/Pods-Hello ZendeskUITests/Pods-Hello ZendeskUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Hello_ZendeskUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Hello_ZendeskUITests
+@end
